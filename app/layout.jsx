3212de3cb08934cdd31809a2c9fc73fbf15a9d51
prajapatiAshutosh15 <ashutosh@getpassionfruit.com>
2026-03-22 03:22:50 +0530

    Add favicon (Scriptly logo)

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500;600&display=swap" rel="stylesheet" />
